befe002b10b4291da72ab72edd1f0c0819d42990
Peter Ajtai <peter.ajtai@thinksolid.com>
2014-10-07 23:31:54 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Playing with zombie.
+
+To try out:
+
+```shell
+npm install
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,24 @@
+var Browser = require("zombie");
+
+require("assert");
+
+// Load the page from localhost
+browser = Browser.create();
+
+browser.visit("http://solid-interactive.github.io/masseuse-examples/two-way-binding/", function (error) {
+
+    try {
+        browser.assert.text('h1', 'Hello world!');
+        console.log(browser.html('h1'));
+
+        browser.fill('input', 'boomy');
+
+        console.log(browser.html('h1'));
+        browser.assert.text('h1', 'boomy');
+    } catch (e) {
+        console.log(e);
+    }
+
+    console.log('~ all done ~');
+    browser.close();
+});
